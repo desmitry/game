fec22734b107f098a7fa6025d919ec4a33547d1c
desmitry <desmitry@users.noreply.github.com>
2026-05-27 10:25:02 +0500

    Add dot prefix to save files

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 TARGET_FPS = 120
-SAVE_PATH = Path("game_save.json")
-BEST_SCORE_PATH = Path("game_best.json")
+SAVE_PATH = Path(".game_save.json")
+BEST_SCORE_PATH = Path(".game_best.json")
